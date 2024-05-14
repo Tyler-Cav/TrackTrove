fe48b28e27fadba71e8ae2f8c13af5424ebb5893
Tyler-Cav <tyler.cavanaugh24@gmail.com>
2024-05-13 22:27:41 -0400

    updated to component out oauth

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,11 @@
 import './App.css'
-import {useEffect, useState} from 'react';
-
+import axios from "axios";
+import { useState } from 'react';
+import Auth from './components/Auth'
 
 function App() {
-  const CLIENT_ID = "cfc55caf7f324ca0ab3ccfb3bb8a90f5"
-  const REDIRECT_URI = `http://localhost:${window.location.port}`
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
-
-  const [token, setToken] = useState("")
-
-  useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
-
-    if (!token && hash) {
-        console.log("token", token)
-        console.log("hash",hash)
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-        console.log("tokenAfter", token)
-
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
-    }
-
-    setToken(token)
-
-  }, [])
-
-  const logout = () => {
-    setToken("")
-    window.localStorage.removeItem("token")
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {!token ? <a
-          className="App-link"
-          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login with Spotify
-        </a> : <button onClick={logout}>Logout</button>
-        }
-      </header>
-    </div>
+    <Auth />
   )
 }
-
 export default App
