6cf3af68cd336422c611cb1c9bf031bd481b116f
shayd3 <ryan.ross215@gmail.com>
2024-05-13 21:52:56 -0400

    Clean up boilerplate. Spotify auth init

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const CLIENT_ID = "cfc55caf7f324ca0ab3ccfb3bb8a90f5"
+  const REDIRECT_URI = `http://localhost:${window.location.port}`
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login with Spotify
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </header>
+    </div>
   )
 }
 
