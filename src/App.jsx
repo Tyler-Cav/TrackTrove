1483b02df3a8a82b511d9e61c7c25e6ca7d50f83
shayd3 <ryan.ross215@gmail.com>
2024-05-16 22:02:33 -0400

    Add scopes, add TODO

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import {useEffect, useState} from 'react';
 
 
 function App() {
+  // TODO: We will need to change this to the Authorization Code flow later... Token based doesn't support refresh tokens.
   const CLIENT_ID = "cfc55caf7f324ca0ab3ccfb3bb8a90f5"
   const REDIRECT_URI = `http://localhost:${window.location.port}`
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
+  const SCOPES = ["user-library-read", "user-follow-read"]
 
   const [token, setToken] = useState("")
 
@@ -38,7 +40,7 @@ function App() {
       <header className="App-header">
         {!token ? <a
           className="App-link"
-          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${encodeURIComponent(SCOPES.join(' '))}`}
           target="_blank"
           rel="noopener noreferrer"
         >
